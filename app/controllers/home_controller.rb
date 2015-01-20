9f4519b4cc56f0f057dc2826cabc097de7c5da13
Matt LaRose <mmdlarose@gmail.com>
2015-01-20 16:54:37 -0500

    Minimum #queries matches

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
 	def index    
     if params[:search]
-      @results = Course.search { fulltext params[:search] }.results
+      @results = Course.search { fulltext params[:search], minimum_match: 1 }.results
     end
   end
   
