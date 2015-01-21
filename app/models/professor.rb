047ff1f70913775dc1ec846b45c9ec86f32d0d27
Matt LaRose <mmdlarose@gmail.com>
2015-01-21 11:36:59 -0500

    Professor to courses method

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,10 +2,13 @@ class Professor < ActiveRecord::Base
 
 	belongs_to :department
 	has_and_belongs_to_many :sections
-	has_and_belongs_to_many :components	
 
-	has_many :courses, through: :components
 	has_many :courses, through: :sections
 
 	validates :name, presence: true
+
+	def courses
+    sections.collect { |s| s.course }
+  end
+  
 end
