5733e2484a5f2dc540a22beaf3ee1f5ee9ba6e8a
Matt LaRose <mmdlarose@gmail.com>
2015-01-23 00:16:53 -0500

    Small home updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
-	def index    
+	def index
+    @pre_fall = Date.today.month >= 3 && Date.today.month <= 9
+    @pre_spring = !@pre_fall
+    
     if params[:search]
       @hits = Course.search { fulltext params[:search], minimum_match: 1 }.each_hit_with_result
     end
